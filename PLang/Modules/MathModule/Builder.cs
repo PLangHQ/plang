5b64620a359ecfd98fc65823cf7ef068e2ee49b3
zach-lawrence-1 <mine268253@gmail.com>
2025-06-30 17:28:48 -0500

    added code from old repo copy to new fork

---
+++ PLang/Modules/MathModule/Builder.cs
@@ -0,0 +1,33 @@
+﻿using PLang.Building.Model;
+using PLang.Errors.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLang.Modules.MathModule
+{
+	public class Builder : BaseBuilder
+	{
+		public Builder() : base()
+		{
+
+		}
+
+		public override async Task<(Instruction?, IBuilderError?)> Build(GoalStep goalStep, IBuilderError? previousBuildError = null)
+		{
+			AppendToSystemCommand(
+				@$"Given a math instruction that is not in a string format, turn it into a string formatted math instruction from the NCalc C# library and passed into the solveExpression function.
+				If a trig function wants to be solved by the user and they ask to find the answer in degrees, convert the output to degrees from radians. If they want to find
+				the value of the trig function with a parameter in degrees, convert it to radians using 3.14 before passing it in. Also convert pi into 3.14 multiplied by whatever number is next to it if applicable.
+				Examples:
+				find 3 plus 4 should turn into 3 + 4. Do this with any operator like *, -, +, /
+				solve for square root of 9 should turn into Sqrt(9)
+				what is 2 raised to the power of 4 should turn into Pow(2, 4)"
+				);
+			//AppendToAssistantCommand("");
+			return await base.Build(goalStep, previousBuildError);
+		}
+	}
+}
--- PLang/Modules/MathModule/Program.cs
+++ PLang/Modules/MathModule/Program.cs
@@ -3,6 +3,8 @@
 using IdGen;
 using Microsoft.CodeAnalysis;
 using Microsoft.Extensions.Logging;
+using NBitcoin.Protocol;
+using NCalc;
 using Newtonsoft.Json.Linq;
 using Org.BouncyCastle.Utilities;
 using PLang.Attributes;
@@ -13,11 +15,13 @@ using PLang.Runtime;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data;
 using System.Linq.Dynamic.Core;
+using System.Linq.Expressions;
 
 namespace PLang.Modules.MathModule
 {
-	[Description("Misc math method")]
+	[Description("Solves math expressions")]
 	public class Program : BaseProgram
 	{
 		public Program() : base()
@@ -25,17 +29,126 @@ namespace PLang.Modules.MathModule
 
 		}
 
-
-		public async Task<(decimal?, IError?)> MultiplyVariableByPower(object variable, int baseValue, int exponent)
+		[Description("Solve a complex math expression given as a string. Please capitalize any functions called like sqrt() into Sqrt()")]
+		public async Task<(double?, IError?)> SolveExpression(string expression)
 		{
-			if (!decimal.TryParse(Convert.ToString(variable), out var numericAmount))
-				return (null, new ProgramError("Could not convert variable to decimal", goalStep, function, FixSuggestion: $"The variable value is: '{variable}' (without quotes)"));
+			//Console.WriteLine("expression: " + expression);
 
-			return (numericAmount * (decimal)Math.Pow(baseValue, exponent), null);
+			if (expression == "")
+				return (null, new ProgramError("Could not use empty variable", goalStep, function, FixSuggestion: $"The variable value is: '{expression}' (without quotes)"));
+
+			var solveFor = new NCalc.Expression(expression);
+			double? value = EvaluateCustomExpressions(solveFor);
+				
+			if (value == null)
+				return (null, new ProgramError("Could not evaluate expression", goalStep, function, FixSuggestion: $"The variable value is: '{expression}' (without quotes)"));
+			
+
+			return (value, null);
 		}
 
+		//Number theory
 
+		[Description("Find the fibonacci number of a given variable or number.")]
+		public async Task<(int?, IError?)> Fibonacci(int variable)
+		{
+			if (variable == 0)
+				return (1, null);
 
+			int value = Convert.ToInt32((1 / Math.Sqrt(5))*(Math.Pow( ((1 + Math.Sqrt(5)) / 2), variable ) - Math.Pow( ((1 - Math.Sqrt(5)) / 2), variable )));
+
+			return (value, null);
+		}
+
+		[Description("Find the first n prime numbers where n is a given number.")]
+		public async Task<(List<int>, IError?)> PrimeNumbers(int number)
+		{
+			//size estimation formula only works for n >= 6
+			if (number <= 5)
+			{
+				switch (number)
+				{
+					case 1:
+						return ([2], null);
+						break;
+					case 2:
+						return ([2, 3], null);
+						break;
+					case 3:
+						return ([2, 3, 5], null);
+						break;
+					case 4:
+						return ([2, 3, 5, 7], null);
+						break;
+					case 5:
+						return ([2, 3, 5, 7, 11], null);
+						break;
+					default:
+						return ([], new ProgramError("Could not convert variable to integer", goalStep, function, FixSuggestion: $"The variable value is: '{number}' (without quotes)"));
+						break;
+				}
+			}
+
+			//formula to find an overestimate for the size of the array needed for Sieve of Eratosthenes to find the first n prime numbers
+			int size = Convert.ToInt32(Math.Ceiling(number * (Math.Log(number) + Math.Log(Math.Log(number)))));
+			Console.WriteLine("Size of array for n = " + number + " is: " + size);
+
+			bool[] isPrimeList = new bool[size];
+
+			//0 and 1 are not prime numbers
+			for (int i = 2; i < isPrimeList.Count(); i++)
+			{
+				isPrimeList[i] = true;
+			}
+
+			//do Sieve of Eratosthenes algorithm
+			for (int i = 0; i < isPrimeList.Count(); i++)
+			{
+				if (i * 2 > isPrimeList.Count())
+				{
+					break;
+				}
+
+				if (isPrimeList[i] == true)
+				{
+					for (int j = i * 2; j < isPrimeList.Count(); j += i)
+					{
+						isPrimeList[j] = false;
+					}
+				}
+			}
+
+			List<int> primeList = new List<int>();
+
+			for (int i = 0; i < isPrimeList.Count(); i++)
+			{
+				if (isPrimeList[i] == true)
+				{
+					primeList.Add(i);
+				}
+
+				if (primeList.Count() == number)
+					break;
+			}
+
+			return (primeList, null);
+		}
+
+		public double? EvaluateCustomExpressions(NCalc.Expression expression)
+		{
+			double value = 0;
+
+			try
+			{
+				value = Convert.ToDouble(expression.Evaluate());
+				value = Math.Round(value, 3, MidpointRounding.AwayFromZero);
+			}
+			catch (EvaluateException e)
+			{
+				return null;
+			}
+
+			return value;
+		}
 	}
 }
-
