436871df654df52cf18e781c6982b938ba3cc0e5
Ingi Gauti <ingig@plang.is>
2025-11-13 10:05:33 +0000

    inital push for ai module

---
+++ PLang/Modules/AiModule/Builder.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using PLang.Building.Model;
+using PLang.Building.Parsers;
+using PLang.Errors;
+using PLang.Errors.Builder;
+using PLang.Models;
+using PLang.Runtime;
+using PLang.Utils;
+using System.Diagnostics;
+
+namespace PLang.Modules.AiModule
+{
+	public class Builder : BaseBuilder
+	{
+		private readonly IEngine engine;
+		private readonly IGoalParser goalParser;
+		private readonly ILogger logger;
+
+		public Builder(IEngine engine, IGoalParser goalParser, PrParser prParser, IMemoryStackAccessor memoryStackAccessor, ILogger logger)
+		{
+			Stopwatch stopwatch = Stopwatch.StartNew();
+			logger.LogDebug($"        - Start constructor for CallGoalModule.Builder - {stopwatch.ElapsedMilliseconds}");
+			this.engine = engine;
+			this.goalParser = goalParser;
+			this.logger = logger;
+			logger.LogDebug($"        - End constructor for CallGoalModule.Builder - {stopwatch.ElapsedMilliseconds}");
+		}
+
+		public record SelectedApp(string Name, string RelativePath);
+		public record SelectedGoal(string Name, string RelativePath);
+		public override async Task<(Instruction?, IBuilderError?)> Build(GoalStep step, IBuilderError? previousBuildError = null)
+		{
+			var goalToCall = new GoalToCallInfo("/modules/AiModule/Builder")
+			{
+				Parameters = new() { ["step"] = step, ["error"] = previousBuildError }
+			};
+
+			var result = await engine.RunGoal(goalToCall, step.Goal, context);
+			return (null, null);
+		}
+
+		public record StepCompiled(string stepText, Dictionary<string, object?> Parameters, List<string> Returns);
+
+
+		public async Task<(Instruction?, IBuilderError?)> BuilderRunApp(GoalStep step, Instruction instruction, GenericFunction gf)
+		{
+
+			//todo: need to fix this valiation
+			return (instruction, null);
+
+		}
+
+	}
+}
+
---
+++ PLang/Modules/AiModule/Program.cs
@@ -0,0 +1,75 @@
+﻿using PLang.Building.Parsers;
+using PLang.Errors;
+using PLang.Errors.Runtime;
+using PLang.Interfaces;
+using PLang.Models;
+using PLang.Runtime;
+using PLang.SafeFileSystem;
+using PLang.Services.AppsRepository;
+using PLang.Utils;
+using System.ComponentModel;
+
+namespace PLang.Modules.AiModule
+{
+
+	public record AiInfo(string model, object config);
+	
+	[Description("Call an App. When the user has the word 'app' in his statement, this should be called. ")]
+	public class Program(IPseudoRuntime pseudoRuntime, IEngine engine, PrParser prParser, IPLangContextAccessor contextAccessor) : BaseProgram()
+	{
+		public PLangContext Context { get { return context; } }
+
+		[Description("Call/Runs another app. app can be located in another directory, then path points the way. goalName is default \"Start\" when it cannot be mapped")]
+		public async Task<(object? Variables, IError? Error)> RunAi(AiInfo aiInfo)
+		{
+			var goalToCall = new GoalToCallInfo("")
+			{
+
+			};
+
+			return await engine.RunGoal(goalToCall, goal, context);
+			/*
+			IEngine newEngine = await engine.GetEnginePool(goal.AbsoluteAppStartupFolderPath).RentAsync(engine, goalStep, appToCall.AppName + "_" + appToCall.Name);
+			var newContext = new PLangContext(memoryStack.Clone(newEngine), newEngine, ExecutionMode.Console);
+			try
+			{
+				if (appToCall.Parameters != null)
+				{
+					foreach (var item in appToCall.Parameters)
+					{
+						if (item.Key.StartsWith("!"))
+						{
+							newContext.AddOrReplace(item.Key, this.memoryStack.LoadVariables(item.Value));
+						}
+						else
+						{
+							newContext.MemoryStack.Put(item.Key, item.Value, goalStep: goalStep);
+						}
+					}
+				}
+
+
+				(var vars, error) = await newEngine.RunGoal(goal, context);
+
+				return (vars, error);
+
+			}
+			catch (Exception ex)
+			{
+				throw;
+			}
+			finally
+			{
+				engine.GetEnginePool(goal.AbsoluteAppStartupFolderPath).Return(newEngine);
+
+			}*/
+		}
+
+
+
+
+	}
+
+
+}
+
